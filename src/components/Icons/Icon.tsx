6043c643409a13cc4e2817c0adbcad1917c21bc9
Nelson costa <canelsong@gmail.com>
2023-11-24 14:53:32 -0400

    now

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -3,18 +3,20 @@ import Copy from "../Code/Copy"
 import Check from "../Code/Check"
 
 
-export default function Icon ({id, ...props }) {
+export default function Icon ({id }:{id: any}) {
 	const icons = { 
 		copy: Copy,
 		check: Check,
 	}
-	const component = icons[id] || null;
+	// const component = icons[id] || null;
+	const myID: "copy" | "check" = id; 
+  	const component = icons[myID] || null;
 
 	if(!component) return null;
 
 	return (
 		<div className={`svg ${id}`}>
-			{ React.createElement(component, props) }
+			{ React.createElement(component) }
 		</div>
 	)
 }
